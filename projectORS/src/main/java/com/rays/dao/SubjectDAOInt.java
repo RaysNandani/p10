25eb4d19da3009d142a05b17a61fcdcb758fc8cd
Nandani kumbhkar <misskumbhkar@gmail.com>
2024-07-30 16:14:02 +0530

    done

---
+++ projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * Nandani kumbhkar 
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
---
+++ projectORS/src/main/java/com/rays/dto/ShoppingCartDTO.java
@@ -0,0 +1,110 @@
+package com.rays.dto;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.rays.common.BaseDTO;
+
+
+@Entity
+@Table(name = "ST_SHOPPINGCART")
+public class ShoppingCartDTO extends BaseDTO {
+	
+	@Column(name = "NAME",length=50)
+	private String name;
+	
+	@Column(name = "PRODUCT",length=50)
+	private String product;
+	
+	@Column(name = "DATE",length=50)
+	private Date date;
+	
+	@Column(name = "QUANTITY",length=50)
+	private Long quantity;
+	
+	
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getProduct() {
+		return product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Long getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return product;
+	}
+
+	@Override
+	public String getUniqueKey() {
+		// TODO Auto-generated method stub
+		return "name";
+	}
+
+	@Override
+	public String getUniqueValue() {
+		// TODO Auto-generated method stub
+		return "name";
+	}
+
+	@Override
+	public String getLabel() {
+		// TODO Auto-generated method stub
+		return "name";
+	}
+
+
+	
+	
+	public LinkedHashMap<String, String> orderBY() {
+		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+		map.put("name", "asc");
+		return map;
+	}
+
+	@Override
+	public LinkedHashMap<String, Object> uniqueKeys() {
+		LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>();
+		map.put("name", name);
+		return map;
+
+	}
+
+
+}
+
+	
+
+
